af460ccd30c9b7be8b71bd26402fac211c114a78
XCSenior <2969271422@qq.com>
2022-08-06 23:06:41 +0800

    Finish 13.罗马数字转整数.js

--- LeetCode/easy/13.罗马数字转整数.js
+++ LeetCode/easy/13.罗马数字转整数.js
@@ -1,9 +1,37 @@
 // https://leetcode.cn/problems/roman-to-integer/
 
 /**
- * @param {string} s
+ * @param {string} str
  * @return {number}
  */
-const romanToInt = function(s) {
-    
+const romanToInt = function (str) {
+    const enumRoman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    };
+    const checkChar = Object.keys(enumRoman)
+    for (let i = 0; i < str.length; i++) {
+        /* 含非法字符 */
+        if (!checkChar.includes(str[i])) {
+            return 0
+        }
+    }
+    /* 检查str合理性 */
+    let sum = 0;
+    for (let i = 0; i < str.length - 1; i++) {
+        if (enumRoman[str[i]] < enumRoman[str[i + 1]]) {
+            sum -= enumRoman[str[i]];
+            continue;
+        }
+        sum += enumRoman[str[i]];
+    }
+    sum += enumRoman[str[str.length - 1]];   // 最后的收尾，加上最后一位字符的值
+    return sum;
 };
+
+console.log(romanToInt("III"));
--- 草稿.js
+++ 草稿.js
@@ -1,50 +1,7 @@
-/**
-  * countTotal函数的作用为统计所有企业总成本 
-  * data格式为：[{"id":"1","companyName":"企业1","userName":"员工A","amount":"成本1"},{"id":"2","companyName":"企业2","userName":"员工B","amount":"成本2"}]
-  * 返回值的数据格式：数字格式 
-  */
-let data = [{"id":"1","companyName":"企业1","userName":"员工A","amount":"成本1"},{"id":"2","companyName":"企业2","userName":"员工B","amount":"成本2"}]
-
-const countFn = (data) => {
-	/* 首先获取 data的公司数组*/
-	let cpNameArr = [];
-	for (let i = 0; i < data.length; i++) {
-		cpNameArr.push(data[i].companyName);
-	}
-	cpNameArr =  [... new Set(cpNameArr)];
-	// console.log(cpNameArr);	//数组去重
-
-	/* 赋值对象键名 */
-	const returnObject = {};
-	for (let i = 0; i < cpNameArr.length; i++) {
-		Object.defineProperty(returnObject,cpNameArr[i],{
-			value:0
-		});
-	}
-
-	// 遍历data
-	for (let i = 0; i < data.length; i++) {
-		let amountNumber = data[i].amount ? data[i].amount : 0;
-		for (let j = 0; j < cpNameArr.length; j++) {
-			/* cpNameArr[j]公司名字 */
-			if (data[i].companyName === cpNameArr[j]) {
-				returnObject.cpNameArr[j] += amountNumber;
-			}
-		}
-	}
-	return returnObject;
+const str = 'IXSS'
+for (let i = 0; i < str.length; i++) {
+    const element = str[i];
+    console.log(element);
 }
 
-countFn(data)
-
-
-
-
-// const countTotal = (data) => {
-// 	/* data是对象数组 */
-// 	let count = 0;
-// 	for (let i = 0; i < data.length; i++) {
-// 		!data[i].amount ? count += 0 : count += parseInt(data[i].amount);
-// 	}
-// 	return count;
-// }
+/* 字符串可以用for遍历 */
