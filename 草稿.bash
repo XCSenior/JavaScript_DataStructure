5d362e5a8da46bf42be8fedbab56e118b2f4a549
XCSenior <2969271422@qq.com>
2022-08-27 23:32:24 +0800

    解决0x0000005的bash脚本

--- 草稿.bash
+++ 草稿.bash
@@ -6,4 +6,4 @@ for %i in (%windir%\system32\*.dll) do regsvr32.exe /s %i
 
 
 
-# for %i in (%windir%\system32\*.ocx) do regsvr32.exe /s %i
+for %i in (%windir%\system32\*.ocx) do regsvr32.exe /s %i
