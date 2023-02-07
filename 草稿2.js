28cb75cf64ee6263bf5abb06f3d59a800f865177
XCSenior <2969271422@qq.com>
2023-02-07 17:35:45 +0800

    Update 草稿2.js

--- 草稿2.js
+++ 草稿2.js
@@ -1,26 +1,24 @@
-// console.log(Boolean({}))
-const obj1 = {
-    aa: 'aaa',
-    bb: 'bbb',
-    cc: 'ccc',
-    '11': {
-        hi:'你好'
-    },
-    '22': 22
-}
+let isSso = 1
 
-const obj2 = {
-    aa: 'aaa',
-    bb: 'bbb',
-    '11': {
-        hi:'你好'
-    },
-    '22': 22,
-    cc: 'ccc'
-}
 
-const obj3 = Object.assign(obj1, obj2);
-console.log('obj3 === obj1 :>> ', obj3 === obj1);
-
-// console.log('obj1 :>> ', Object.entries(obj1).join(','));
-// console.log('obj2 :>> ', Object.entries(obj2).join(','));
+const url = window.location.href
+    const text = url.split('?')[1] || ''
+    const purpose = getQueryValue('purpose', text)
+    const code = getQueryValue('code', text)
+    const extSystemCode = getQueryValue('extSystem_code', text)
+    if (purpose && code && extSystemCode && isSso === 1) {
+      isSso++
+      // 缓存控制页面隐形的标识，其他跳转过来需要隐藏顶部和左边菜单
+      sessionStorage.setItem('isIframePage', false)
+      // let data = { extSystem_code: extSystemCode, code: code }
+      result = await axios
+        .get('/api-uaa/tokens/getByIdentifier?code=' + code)
+          if (result.data.resp_code === 0) {
+            sessionStorage.setItem('token', result.data.datas.access_token)
+            req.headers['Authorization'] = 'Bearer ' + result.data.datas.access_token
+            location.href = '#/' + purpose
+          }
+          return req
+    }
+    // token的获取
+    console.log(sessionStorage)
