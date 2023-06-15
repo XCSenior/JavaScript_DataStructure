04cc25cac9b2b1cb055ca92489add5b770325e0b
XCSenior <2969271422@qq.com>
2023-06-15 20:46:45 +0800

    Create Array index.js.

---
+++ data_structure/1.Array/index.js
@@ -0,0 +1,23 @@
+// 声明
+const arr = new Array(1, 2, 3);
+// 字面量声明
+const arr2 = [1, 2, 3];
+
+// 根据索引值来进行元素的访问
+arr[0];
+
+/* 1、遍历所有元素：for循环 */
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+}
+
+/* 2、数组方法：pop、push、shift、unshift */
+
+
+
+
+
+
+
+
+
