45b2221569416aa83f936388a33e1d3eea3773af
XCSenior <2969271422@qq.com>
2023-07-24 12:58:48 +0800

    feat: update the base framework of DoublyLinkedList.

--- data_structure/4.链表/双向链表/index.js
+++ data_structure/4.链表/双向链表/index.js
@@ -8,6 +8,7 @@
  * ? 5、clear：清空链表
  * ? 6、size属性：链表的长度
  * ? 7、getHead：获取链表头
+ * ? 8、getTail：获取链表尾
  */
 /**
  * @class DoublyNode1 普通双向链表封装的结点
@@ -24,4 +25,100 @@ var DoublyNode1 = /** @class */ (function () {
     }
     ;
     return DoublyNode1;
+}());
+var DoublyLinkedList = /** @class */ (function () {
+    function DoublyLinkedList() {
+        // 单链表的长度
+        this.count = 0;
+        // 单链表的表头，表尾元素
+        this.head = null;
+        this.tail = null;
+    }
+    // 1、插入元素至链尾
+    DoublyLinkedList.prototype.push = function (data) {
+    };
+    ;
+    // 编写公用方法
+    // 公用方法1、获取指定位置的结点
+    DoublyLinkedList.prototype.getNodeAt = function (targetIndex) {
+        if (targetIndex >= 0 && targetIndex < this.count) {
+            var currentNode = this.head;
+            // 开始遍历查找目标节点
+            for (var i = 0; i < targetIndex; i++) {
+                currentNode = currentNode.next;
+            }
+            // 循环结束，currentNode就是要被删除的结点
+            return currentNode;
+        }
+    };
+    ;
+    // 公用方法2、查找目标元素值的结点索引值
+    DoublyLinkedList.prototype.findIndexOf = function (targetData) {
+        var currentNode = this.head;
+        // 开始遍历查找目标节点
+        for (var i = 0; i < this.count; i++) {
+            if (currentNode.data === targetData) {
+                return i;
+            }
+            else {
+                currentNode = currentNode.next;
+            }
+        }
+        return -1;
+    };
+    // 2、（1）指定索引删除
+    DoublyLinkedList.prototype.removeAt = function (targetIndex) {
+        if (targetIndex >= 0 && targetIndex < this.count) {
+            var currentNode = void 0;
+            if (targetIndex === 0) { // 删除头部元素
+                this.head = this.head.next;
+            }
+            else {
+                // 开始遍历查找目标节点
+                var previousNode = this.getNodeAt(targetIndex - 1);
+                // 循环结束，currentNode就是要被删除的结点
+                currentNode = previousNode.next;
+                previousNode.next = currentNode.next;
+            }
+            --this.count; // 删除成功后，总数减一
+            return currentNode.data;
+        }
+    };
+    ;
+    // 3、（2）指定删除元素值相同的结点
+    DoublyLinkedList.prototype.removeEqual = function (targetData) {
+        var targetIndex = this.findIndexOf(targetData);
+        return this.removeAt(targetIndex) ? true : false;
+    };
+    ;
+    // 4、（3）任意位置上插入元素
+    DoublyLinkedList.prototype.insertTo = function (data, targetIndex) {
+        return false;
+    };
+    ;
+    Object.defineProperty(DoublyLinkedList.prototype, "isEmpty", {
+        // 5、（4）判断是否为空链表
+        get: function () { return !this.head; },
+        enumerable: false,
+        configurable: true
+    });
+    ;
+    // 6、（5）清空链表
+    DoublyLinkedList.prototype.clear = function () {
+        this.head = null;
+        this.tail = null;
+        return true;
+    };
+    Object.defineProperty(DoublyLinkedList.prototype, "size", {
+        // 7、（6）访问私有属性链表长度
+        get: function () { return this.count; },
+        enumerable: false,
+        configurable: true
+    });
+    ;
+    // 8、（7）获取链表头head
+    DoublyLinkedList.prototype.getHead = function () {
+        return this.head;
+    };
+    return DoublyLinkedList;
 }());
--- data_structure/4.链表/双向链表/index.ts
+++ data_structure/4.链表/双向链表/index.ts
@@ -8,6 +8,7 @@
  * ? 5、clear：清空链表
  * ? 6、size属性：链表的长度
  * ? 7、getHead：获取链表头
+ * ? 8、getTail：获取链表尾
  */
 /**
  * @class DoublyNode1 普通双向链表封装的结点
@@ -25,4 +26,87 @@ class DoublyNode1<T> {
         this.prev = null;
         this.next = null;
     };
+}
+
+class DoublyLinkedList<T> {
+    // 单链表的长度
+    protected count: number = 0;
+    // 单链表的表头，表尾元素
+    public head: DoublyNode1<T> = null;
+    public tail: DoublyNode1<T> = null;
+
+    // 1、插入元素至链尾
+    public push(data: T){
+
+    };
+
+    // 编写公用方法
+    // 公用方法1、获取指定位置的结点
+    public getNodeAt(targetIndex: number): SingleNode<T> | void{
+        if (targetIndex >= 0 && targetIndex < this.count) {
+            let currentNode: SingleNode<T> = this.head;
+            // 开始遍历查找目标节点
+            for (let i = 0; i < targetIndex; i++) {
+                currentNode = currentNode.next;
+            }
+            // 循环结束，currentNode就是要被删除的结点
+            return currentNode;
+        }
+    };
+    // 公用方法2、查找目标元素值的结点索引值
+    public findIndexOf(targetData: T): number {
+        let currentNode: SingleNode<T> = this.head;
+        // 开始遍历查找目标节点
+        for (let i = 0; i < this.count; i++) {
+            if (currentNode.data === targetData) {
+                return i;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+        return -1;
+    }
+    // 2、（1）指定索引删除
+    public removeAt(targetIndex: number): T | void{
+        if (targetIndex >= 0 && targetIndex < this.count) {
+            let currentNode: SingleNode<T>;
+            if (targetIndex === 0) { // 删除头部元素
+                this.head = this.head.next;
+            } else {
+                // 开始遍历查找目标节点
+                const previousNode = <SingleNode<T>>this.getNodeAt(targetIndex - 1);
+                // 循环结束，currentNode就是要被删除的结点
+                currentNode = previousNode.next;
+                previousNode.next = currentNode.next;
+            }
+            --this.count; // 删除成功后，总数减一
+            return currentNode.data;
+        }
+    };
+    // 3、（2）指定删除元素值相同的结点
+    public removeEqual(targetData: T): boolean{
+        const targetIndex = this.findIndexOf(targetData);
+        return this.removeAt(targetIndex) ? true : false;
+    };
+    // 4、（3）任意位置上插入元素
+    public insertTo(data: T, targetIndex: number): boolean{
+        return false;
+    };
+
+    // 5、（4）判断是否为空链表
+    public get isEmpty(): boolean { return !this.head; };
+
+    // 6、（5）清空链表
+    public clear(): boolean{
+        this.head = null;
+        this.tail = null;
+        return true;
+    }
+    // 7、（6）访问私有属性链表长度
+    public get size(): number { return this.count; };
+
+    // 8、（7）获取链表头head
+    public getHead(): DoublyNode1<T> | null {
+        return this.head;
+    }
 }
