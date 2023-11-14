538e7c29c795444b64184a8892e3885ce4612d8e
XCSenior <2969271422@qq.com>
2023-11-14 18:07:05 +0800

    feat：BST tree exercise file framework established.

---
+++ data_structure/7.树/二叉搜索树/index.js
@@ -0,0 +1,14 @@
+/**
+ * 二叉搜索树
+ *
+ */
+/* 二叉搜索树结点类型 */
+var bstTreeNode = /** @class */ (function () {
+    function bstTreeNode(nodeValue) {
+        this.keyNode = nodeValue;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+    ;
+    return bstTreeNode;
+}());
---
+++ data_structure/7.树/二叉搜索树/index.ts
@@ -0,0 +1,22 @@
+/**
+ * 二叉搜索树
+ *
+ */
+
+/* 二叉搜索树结点类型 */
+class bstTreeNode<T> {
+    keyNode: T;
+    leftNode: T;
+    rightNode: T;
+    constructor(nodeValue: T) {
+        this.keyNode = nodeValue;
+        this.leftNode = null;
+        this.rightNode = null;
+    };
+
+
+}
+
+
+
+
